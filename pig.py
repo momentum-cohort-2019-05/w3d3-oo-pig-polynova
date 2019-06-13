5928401e9caeb97b2ed3b73b89acf631920ec421
polynova <jocelyncasanova1@gmail.com>
2019-06-13 13:00:21 -0400

    attempted skeleton

---
+++ pig.py
@@ -0,0 +1,38 @@
+import random 
+
+class Game:
+    def _init_(self, player):
+        self.player = player
+
+class Player1:
+        def _init_(self):
+
+
+class Player2:
+    def _init_(self):
+
+
+class Die:
+    def roll(self):
+        roll_result = randint(1,6)
+    print("This is what you got" + roll_result)
+
+     
+
+    # def _init_(self):
+        
+
+    # def _good_roll_(self):
+        if self.side in self._good_roll_:
+            return self.side
+            
+    # def _bad_roll_(self):
+
+# variable.randint(1,6)
+#make a variable that stores random inter
+
+class Score:
+
+
+
+return 
